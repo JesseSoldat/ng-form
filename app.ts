09749bf94efc623872ac916a5da2af1c4dfcd03a
jesse soldat <jsoldat@hotmail.com>
2016-11-16 14:33:23 +0700

    form validators

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 import {Component} from 'angular2/core';
 import {bootstrap} from 'angular2/platform/browser';
-import { FORM_DIRECTIVES, FormBuilder, ControlGroup } from 'angular2/common';
-//130
+import { FORM_DIRECTIVES, CORE_DIRECTIVES, FormBuilder, ControlGroup, Validators, AbstractControl} from 'angular2/common';
+//141
 
 @Component({
 	selector: 'forms-demo-app',
@@ -64,5 +64,52 @@ class FormSkuBuilder {
 	}
 }
 
+@Component({
+	selector: 'form-validations',
+	directives: [FORM_DIRECTIVES, CORE_DIRECTIVES],
+	template: `
+	<div class="ui raised segment">
+		<h2 class="ui header">Demo Form: with Validations</h2>
+		<form [ngFormModel]="myForm"
+				(ngSubmit)="onSubmit(myForm.value)"
+				class="ui form">
 
-bootstrap(FormSkuBuilder);
+				<div class="field"
+						[class.error]="!sku.valid && sku.touched">
+						<label for="skuInput">SKU</label>
+						<input type="text"
+								id="skuInput"
+								placeholder="SKU"
+								[ngFormControl]="sku">
+						<div *ngIf="!sku.valid"
+							class="ui error message">SKU is invalid
+						</div>
+				</div>
+
+				<div *ngIf="!myForm.valid"
+						class="ui error message">Form is invalid
+				</div>
+				<button type="submit" class="ui button">Submit</button>
+				
+		</form>
+	</div>
+	`
+})
+class FormValidations {
+	myForm: ControlGroup;
+	sku: AbstractControl;
+
+	constructor(fb: FormBuilder) {
+		this.myForm = fb.group({
+			'sku': ['', Validators.required]
+		});
+
+		this.sku = this.myForm.controls['sku'];
+	}
+	onSubmit(value: string): void {
+		console.log('you submitted value: ', value);
+	}
+} 
+
+
+bootstrap(FormValidations);
